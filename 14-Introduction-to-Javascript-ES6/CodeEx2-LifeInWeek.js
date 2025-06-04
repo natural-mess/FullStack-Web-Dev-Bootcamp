745f2ec7a5a81710a7aaad63cd4f949993ae8de9
natural-mess <37369211+natural-mess@users.noreply.github.com>
2025-06-04 16:16:55 +0200

    Finished section 14

---
+++ 14-Introduction-to-Javascript-ES6/CodeEx2-LifeInWeek.js
@@ -0,0 +1,14 @@
+function lifeInWeeks(age) {
+    
+/************Don't change the code above************/    
+    
+    //Write your code here.
+    var yearLeft = 90-age;
+    console.log("You have " + yearLeft*365 + " days, " + yearLeft*52 + " weeks, and " + yearLeft*12 + " months left.");
+    
+    
+    
+    
+/*************Don't change the code below**********/
+}
+
---
+++ 14-Introduction-to-Javascript-ES6/stringCasting.js
@@ -0,0 +1,3 @@
+var myName = prompt("What is your name?");
+var firstChar = myName.slice(0,1).toUpperCase();
+alert(firstChar + myName.slice(1,myName.length).toLowerCase());
