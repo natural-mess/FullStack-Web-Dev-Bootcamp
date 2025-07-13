a39615bd5b211c7b8183106a33d9c429b6deac3e
Natural Mess <37369211+natural-mess@users.noreply.github.com>
2025-07-13 19:50:24 +0200

    Finish section 28

--- 28-ApplicationProgrammingInterfaces(APIs)/28.6 Secrets Project/index.js
+++ 28-ApplicationProgrammingInterfaces(APIs)/28.6 Secrets Project/index.js
@@ -11,3 +11,31 @@
 // secret and the username of the secret.
 
 // 6. Listen on your predefined port and start the server.
+
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com";
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get(`${API_URL}/random`);
+    res.render("index.ejs", {
+      secret: result.data.secret,
+      user: result.data.username,
+    });
+  } catch (error) {
+    console.log(error.response.data);
+    res.status(500);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
